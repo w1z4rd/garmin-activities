a0c21ec010125c094b113b98d87468e377b876bb
Costel Radulescu <wizard.ro@gmail.com>
2015-08-02 18:02:50 +0300

    Inital commit

---
+++ src/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+current_dir=`pwd`
+cd /tmp
+sudo apt-get install python-setuptools
+git clone https://github.com/Tigge/openant.git
+cd openant
+sudo python setup.py install
+git clone https://github.com/Tigge/antfs-cli.git
+cd antfs-cli
+sudo python setup.py install
+cd $current_dir
