71f9c57c044ed5db62b159aae306c71b20ebff60
Costel Radulescu <wizard.ro@gmail.com>
2015-09-29 13:18:51 +0300

    cloning antfs-ci in /tmp not /tmp/openant

--- src/install.sh
+++ src/install.sh
@@ -5,6 +5,7 @@ sudo apt-get install python-setuptools
 git clone https://github.com/Tigge/openant.git
 cd openant
 sudo python setup.py install
+cd ..
 git clone https://github.com/Tigge/antfs-cli.git
 cd antfs-cli
 sudo python setup.py install
